b30e9e7a04664c78d44c932d6fbecf2f90a73a04
Fred Brossard <fred.b.devweb@gmail.com>
2023-02-17 18:57:42 +0100

    installing Prisma

---
+++ prisma/client.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+declare global{
+    namespace NodeJS{
+        interface Global {}
+    }
+}
+
+//add prisma to the NodeJS global type
+interface CustomNodeJSGlobal extends NodeJS.Global {
+    prisma: PrismaClient
+}
+
+//Preevent multiple instances of Prisma Client in development
+declare const global: CustomNodeJSGlobal;
+
+
+ const prisma = global.prisma || new PrismaClient()
+
+if (process.env.NODE_ENV === "development") global.prisma = prisma
+
+export default prisma
